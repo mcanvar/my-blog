70791e4e291280e1e83d0610e20471f04205543a
Mevlüt Canvar <mevlutcanvar@gmail.com>
2021-06-01 03:05:44 +0300

    adds menu icons

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,59 +1,73 @@
 <script>
-	export let segment;
+    export let segment;
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
+    nav {
+        border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+        font-weight: 300;
+        padding: 0 1em;
+    }
 
-	ul {
-		margin: 0;
-		padding: 0;
-	}
+    ul {
+        margin: 0;
+        padding: 0;
+    }
 
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
+    /* clearfix */
+    ul::after {
+        content: '';
+        display: block;
+        clear: both;
+    }
 
-	li {
-		display: block;
-		float: left;
-	}
+    li {
+        display: block;
+        float: left;
+    }
 
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
+    [aria-current] {
+        position: relative;
+        display: inline-block;
+    }
 
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
+    [aria-current]::after {
+        position: absolute;
+        content: '';
+        width: calc(100% - 1em);
+        height: 2px;
+        background-color: rgb(255, 62, 0);
+        display: block;
+        bottom: -1px;
+    }
 
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
+    a {
+        text-decoration: none;
+        padding: 1em 0.5em;
+        display: block;
+    }
 </style>
 
 <nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
+    <ul>
+        <li>
+            <a aria-current="{segment === undefined ? 'page' : undefined}" href=".">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                    <path class="heroicon-ui"
+                          d="M13 20v-5h-2v5a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-7.59l-.3.3a1 1 0 1 1-1.4-1.42l9-9a1 1 0 0 1 1.4 0l9 9a1 1 0 0 1-1.4 1.42l-.3-.3V20a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zm5 0v-9.59l-6-6-6 6V20h3v-5c0-1.1.9-2 2-2h2a2 2 0 0 1 2 2v5h3z"/>
+                </svg>
+            </a>
+        </li>
 
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
+        <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+             the blog data when we hover over the link or tap it on a touchscreen -->
+        <li>
+            <a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                    <path class="heroicon-ui"
+                          d="M20 11.46V20a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2v-4h-2v4a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-8.54A4 4 0 0 1 2 8V7a1 1 0 0 1 .1-.45l2-4A1 1 0 0 1 5 2h14a1 1 0 0 1 .9.55l2 4c.06.14.1.3.1.45v1a4 4 0 0 1-2 3.46zM18 12c-1.2 0-2.27-.52-3-1.35a3.99 3.99 0 0 1-6 0A3.99 3.99 0 0 1 6 12v8h3v-4c0-1.1.9-2 2-2h2a2 2 0 0 1 2 2v4h3v-8zm2-4h-4a2 2 0 1 0 4 0zm-6 0h-4a2 2 0 1 0 4 0zM8 8H4a2 2 0 1 0 4 0zm11.38-2l-1-2H5.62l-1 2h14.76z"/>
+                </svg>
+            </a>
+        </li>
+    </ul>
 </nav>
