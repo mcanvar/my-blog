27711a8099bb155d12360ca11baffe8150302af4
Mevlut Canvar <mevlutcanvar@gmail.com>
2022-05-31 17:12:15 +0300

    implements query overriding for listing to get better perf.

---
+++ src/graphql/overrides/queries.ts
@@ -0,0 +1,26 @@
+export const postsByDate = /* GraphQL */ `
+  query PostsByDate(
+    $language: String!
+    $sortDirection: ModelSortDirection
+    $filter: ModelPostFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    postsByDate(
+      language: $language
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        language
+        title
+        slug
+        description
+        createdAt
+      }
+      nextToken
+    }
+  }
+`
